8fc5101134124b60854de3ae8510d234d8a43c18
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2025-03-17 04:10:56 +0300

    Fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,71 @@
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import {
+	isYouTubeProvider,
+	MediaPlayer,
+	MediaPlayerInstance,
+	MediaProvider,
+	MediaProviderAdapter,
+	Poster,
+	useMediaState,
+} from '@vidstack/react';
 import {
 	PlyrLayout,
 	plyrLayoutIcons,
 } from '@vidstack/react/player/layouts/plyr';
 
 const PlayerContent = () => {
+	const [showControls, setShowControls] = useState<boolean>(true);
+	const playerRef = useRef<MediaPlayerInstance>(null);
 	const query = useSearchParams();
 	const videoId = query.get('videoId');
+	const isStarted = useMediaState('started', playerRef);
+
+	useEffect(() => {
+		if (playerRef.current) {
+			playerRef.current?.startLoading();
+		}
+	}, [videoId]);
 
 	if (!videoId) return <p>Loading...</p>;
 
-	console.log({ videoId });
+	function onProviderChange(provider: MediaProviderAdapter | null) {
+		if (isYouTubeProvider(provider)) {
+			provider.cookies = true;
+		}
+	}
 
 	return (
 		<div className='aspect-video relative bg-black overflow-hidden shadow-2xl group'>
 			<MediaPlayer
+				ref={playerRef}
+				onProviderChange={onProviderChange}
 				aspectRatio='16/9'
-				autoPlay={true}
+				load='eager'
 				src={`youtube/${videoId}`}
+				crossOrigin='anonymous'
+				autoPlay={true}
+				controls={showControls}
+				playsInline
+				onLoadedData={() => {
+					setShowControls(false);
+				}}
 			>
-				<MediaProvider />
+				<MediaProvider>
+					<Poster
+						hidden={!isStarted}
+						className='vds-poster z-50'
+						src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
+						alt='Girl walks into campfire with gnomes surrounding her friend ready for their next meal!'
+					/>
+				</MediaProvider>
+
 				<PlyrLayout
 					icons={plyrLayoutIcons}
 					displayDuration
 					controls={[
-						'pip',
 						'play',
 						'progress',
 						'current-time',
